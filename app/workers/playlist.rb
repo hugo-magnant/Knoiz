52c16e4151dd0890ef7dddad1836de9c31a7ac1d
Hugo Magnant <hugo.magnant@gmail.com>
2023-02-23 16:20:14 +0100

    a

--- app/workers/playlist.rb
+++ app/workers/playlist.rb
@@ -3,79 +3,114 @@ class Playlist
   
     def perform(text_search, spotify_user_content, spotify_user_id)
 
-        @retry_count ||= 0
+        begin
 
-        puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
-        puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
-        puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
-        puts @retry_count
-        puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
-        puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
-        puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
+            @retry_count ||= 0
 
-        require 'base64'
-
-        current_user = User.find_by(id: spotify_user_id)
-    
-        image_data = File.read("app/assets/images/dlogo.jpeg")
-        encoded_image_data = Base64.strict_encode64(image_data)
-
-        spotify_user = RSpotify::User.new(spotify_user_content)
-
-        if @retry_count > 0
-            @playlist.delete!
             puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
             puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
             puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
-            puts "playlist must be deleted"
+            puts @retry_count
             puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
             puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
             puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
-            @retry_count = 0
-        end
 
-        client = OpenAI::Client.new(access_token: ENV['OPENAI_KEY'])
+            require 'base64'
 
-        response_playlist_title = client.completions(
-            parameters: {
-                model: "text-davinci-003",
-                prompt: "Create a playlist title that matches this description : \"#{text_search}\"",
-                temperature: 0.7,
-                max_tokens: 1000,
-                top_p: 1,
-                frequency_penalty: 0,
-                presence_penalty: 0
-            })
-
-        playlist_title = response_playlist_title["choices"].map { |c| c["text"] }
+            current_user = User.find_by(id: spotify_user_id)
         
-        playlist_title = playlist_title.to_s
-        playlist_title = playlist_title.gsub!("\\n", "")
-        playlist_title = playlist_title.gsub!("\"", "")
-        playlist_title = playlist_title.gsub!("[", "")
-        playlist_title = playlist_title.gsub!("]", "")
-        playlist_title = playlist_title.gsub!("\\", "")
+            image_data = File.read("app/assets/images/dlogo.jpeg")
+            encoded_image_data = Base64.strict_encode64(image_data)
 
-        response = client.completions(
-            parameters: {
-                model: "text-davinci-003",
-                prompt: "Create in one column : a playlist of strictly 30 songs made by strictly 30 different artists in the same style and the same era of #{text_search}. Don't put the same music more than once. Items must be separated by |. Song and artist must be separate by -. Don't write the feats, only the main music artist.",
-                temperature: 0.7,
-                max_tokens: 1000,
-                top_p: 1,
-                frequency_penalty: 0,
-                presence_penalty: 0
-            })
+            spotify_user = RSpotify::User.new(spotify_user_content)
 
+            if @retry_count > 0
+                @playlist.delete!
+                puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
+                puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
+                puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
+                puts "playlist must be deleted"
+                puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
+                puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
+                puts "!''''''''''''''''!'!'!'!'!!!!!!!!!!!!!!!'!'!!#############################"
+                @retry_count = 0
+            end
+
+            client = OpenAI::Client.new(access_token: ENV['OPENAI_KEY'])
+
+            response_playlist_title = client.completions(
+                parameters: {
+                    model: "text-davinci-003",
+                    prompt: "Create a playlist title that matches this description : \"#{text_search}\"",
+                    temperature: 0.7,
+                    max_tokens: 1000,
+                    top_p: 1,
+                    frequency_penalty: 0,
+                    presence_penalty: 0
+                })
+
+            playlist_title = response_playlist_title["choices"].map { |c| c["text"] }
+            
+            playlist_title = playlist_title.to_s
+            playlist_title = playlist_title.gsub!("\\n", "")
+            playlist_title = playlist_title.gsub!("\"", "")
+            playlist_title = playlist_title.gsub!("[", "")
+            playlist_title = playlist_title.gsub!("]", "")
+            playlist_title = playlist_title.gsub!("\\", "")
+
+            response = client.completions(
+                parameters: {
+                    model: "text-davinci-003",
+                    prompt: "Create in one column : a playlist of strictly 30 songs made by strictly 30 different artists in the same style and the same era of #{text_search}. Don't put the same music more than once. Items must be separated by |. Song and artist must be separate by -. Don't write the feats, only the main music artist.",
+                    temperature: 0.7,
+                    max_tokens: 1000,
+                    top_p: 1,
+                    frequency_penalty: 0,
+                    presence_penalty: 0
+                })
+
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts response
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+                puts "_________________________________________________________"
+
+            reponse_ai = response["choices"].map { |c| c["text"] }
+            reponse_ai = reponse_ai.to_s
+            reponse_ai = reponse_ai.gsub!("\\n", "")
+            reponse_ai = reponse_ai.gsub!("\"", "")
+            reponse_ai = reponse_ai.gsub!("[", "")
+            reponse_ai = reponse_ai.gsub!("]", "")
+            
+            @myArray = reponse_ai.split("|")
+
+
+            temp_playlist = []
+
+            for song in @myArray do
+
+                track = RSpotify::Track.search(song).first
+                temp_playlist.append(track)
+
+            end
+
+            @playlist = spotify_user.create_playlist!("Spotilab.ai | #{playlist_title}")
+            @playlist.replace_image!(encoded_image_data, 'image/jpeg')
             puts "_________________________________________________________"
             puts "_________________________________________________________"
             puts "_________________________________________________________"
             puts "_________________________________________________________"
             puts "_________________________________________________________"
             puts "_________________________________________________________"
-            puts response
-            puts "_________________________________________________________"
-            puts "_________________________________________________________"
+            puts temp_playlist
             puts "_________________________________________________________"
             puts "_________________________________________________________"
             puts "_________________________________________________________"
@@ -83,52 +118,31 @@ class Playlist
             puts "_________________________________________________________"
             puts "_________________________________________________________"
 
-        reponse_ai = response["choices"].map { |c| c["text"] }
-        reponse_ai = reponse_ai.to_s
-        reponse_ai = reponse_ai.gsub!("\\n", "")
-        reponse_ai = reponse_ai.gsub!("\"", "")
-        reponse_ai = reponse_ai.gsub!("[", "")
-        reponse_ai = reponse_ai.gsub!("]", "")
+            @playlist.add_tracks!(temp_playlist)
+            @playlist_id = @playlist.id
+
+            current_user.wallet.credits -= 1
+            current_user.wallet.save
+
+        rescue Exception => e
+
+            puts "Fail Fail Fail Fail Fail Fail Fail FailFail Fail FailFail"
+            puts "Fail Fail Fail Fail Fail Fail Fail FailFail Fail FailFail"
+            puts "Fail Fail Fail Fail Fail Fail Fail FailFail Fail FailFail"
+            puts "Fail Fail Fail Fail Fail Fail Fail FailFail Fail FailFail"
+            puts "Fail Fail Fail Fail Fail Fail Fail FailFail Fail FailFail"
+
+        ensure
+
+            @retry_count += 1
+
+            puts "00000000000000000000000000000000000000000000000000000000000000000000000000"
+            puts "00000000000000000000000000000000000000000000000000000000000000000000000000"
+            puts "00000000000000000000000000000000000000000000000000000000000000000000000000"
+            puts "00000000000000000000000000000000000000000000000000000000000000000000000000"
+            puts "00000000000000000000000000000000000000000000000000000000000000000000000000"
         
-        @myArray = reponse_ai.split("|")
-
-
-        temp_playlist = []
-
-        for song in @myArray do
-
-            track = RSpotify::Track.search(song).first
-            temp_playlist.append(track)
-
         end
-
-        @playlist = spotify_user.create_playlist!("Spotilab.ai | #{playlist_title}")
-        @playlist.replace_image!(encoded_image_data, 'image/jpeg')
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts temp_playlist
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-        puts "_________________________________________________________"
-
-        @playlist.add_tracks!(temp_playlist)
-        @playlist_id = @playlist.id
-
-        current_user.wallet.credits -= 1
-        current_user.wallet.save
-
-    ensure
-
-        @retry_count += 1
 
     end
 
