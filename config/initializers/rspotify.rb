18452b90fa8a25ec405bff0489db3f22a15f3c72
Hugo Magnant <hugo.magnant@gmail.com>
2023-03-01 10:36:43 +0100

    Delete modify private scope

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -4,5 +4,5 @@ OmniAuth.config.silence_get_warning = true
 OmniAuth.config.allowed_request_methods = [:post, :get]
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_KEY'], scope: 'user-top-read user-read-email playlist-modify-public playlist-modify-private ugc-image-upload user-read-recently-played'
+  provider :spotify, ENV['SPOTIFY_ID'], ENV['SPOTIFY_KEY'], scope: 'user-top-read user-read-email playlist-modify-public ugc-image-upload user-read-recently-played'
 end
